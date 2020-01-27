7dbb7c9dc4c891fe1590505a2796eddbeac7f76a
Jay <jay.jay0088@gmail.com>
2020-01-27 13:41:27 -0500

    merged protothreading Drive.ino

--- Drive/Drive.ino
+++ Drive/Drive.ino
@@ -1,39 +1,164 @@
+#include<pt.h>
 
-// defines pins numbers
-const int stepPin1 = 2;
+const int stepPin1 = 2;   //Motor control pins
 const int dirPin1 = 6;
 const int stepPin2 = 4;
 const int dirPin2 = 5;
-
-char receive[31];
-
-
-int motor1_dir;
+char receive[31];         //serial receive variable
+int motor1_dir;           //variable to store the motor direction data
 int motor2_dir;
-const int en = 7;
-
-const int trigPin = 10;
+const int en = 7;         //motor enable pin : high turns the motors off
+const int trigPin = 10;   //Ultrasonic control pins
 const int echoPin = 11;
 const int trigPin2 = 12;
 const int echoPin2 = 13;
-
-
-int distThresh = 25;
-
-long duration;
+int distThresh = 25;      // distance threshold to stop Flora
+long duration;            //duration of US pulse, and distance variables
 long duration2;
 int distance1;
-int distance2;
-int count = 0;
+int distance2;       
+int comm[3];              //variable to hold the parsed serial command
+int num_steps;            //number of steps to take
+int steps_left;           //steps remaning to take
+int steps_start;          //number of steps to begin with
+int ramp_steps;           //number of steps to ramp the speed up and down with
+unsigned int delayTime;   //the length of pause between each step
 
-int comm[3];
+static struct pt pt1, pt2, pt3;   //protothread structures
 
-int num_steps;
-int steps_left;
-int steps_start;
-int ramp_steps;
-unsigned int delayTime;
-unsigned long timeStamp1, timeStamp2;
+static int protoUS1(struct pt *pt) {
+  static unsigned long timeStamp = 0;
+  PT_BEGIN(pt);         //the first ultrasonic code
+  while (1) {
+    timeStamp = millis();
+    PT_WAIT_UNTIL(pt, millis() - timeStamp > 415);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH, 3000);
+    distance1 = duration / 2 * 0.034;
+    Serial.println(distance1);
+  }
+  PT_END(pt);
+}
+
+static int protoUS2(struct pt *pt) {
+  static unsigned long timeStamp = 0;
+  PT_BEGIN(pt);         //the seconds ultrasonic code
+  while (1) {
+    timeStamp = millis();
+    PT_WAIT_UNTIL(pt, millis() - timeStamp > 400);
+    digitalWrite(trigPin2, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin2, LOW);
+    duration2 = pulseIn(echoPin2, HIGH, 30000);
+    distance2 = duration2 / 2 * 0.034;
+    Serial.println(distance2);
+  }
+  PT_END(pt);
+}
+
+static int protoMain(struct pt *pt) {
+  static unsigned long timeStamp = 0;
+  PT_BEGIN(pt);
+  while (1) {
+    if (Serial.available()) {
+      byte sizeR = Serial.readBytes(receive, 31);
+      receive[sizeR] = 0;
+      char* command = strtok(receive, ",");
+      int i = 0;
+      while (command != 0)
+      {                           //parses the received serial communication
+                                  //into three separate variables, separated by ","
+        comm[i] = atoi(command);
+        i += 1;
+        command = strtok(0, ",");
+      }
+    }
+
+    if (comm[0] == 0) {  //sets the motor directions dependent on the serial input
+      if (comm[1] == 1) {
+        motor1_dir = LOW;
+        motor2_dir = HIGH;
+      }
+      else if (comm[1] == 0) {
+        motor1_dir = HIGH;
+        motor2_dir = LOW;
+      }
+    }
+    else if (comm[0] == 1) {
+      if (comm[1] == 1) {
+        motor1_dir = HIGH;
+        motor2_dir = HIGH;
+      }
+      else if (comm[1] == 0) {
+        motor1_dir = LOW;
+        motor2_dir = LOW;
+      }
+    }
+
+    num_steps = comm[2];
+    comm[2] = 0;
+    steps_start = num_steps;
+    delayTime = 15000;
+    if (num_steps < 200) {  //used when the number of steps is less than the default
+      ramp_steps = num_steps / 2; // number of steps in the ramp functions
+    }
+    else {
+      ramp_steps = 100;
+    }
+
+    while (num_steps > 0) {
+      if (comm[0] == 1) {
+        delayTime = 30000; 
+      }
+      else if (comm[0] == 0) {  //ramps up and down or sets the constant delay
+        if (num_steps + ramp_steps > steps_start) {
+          delayTime = delayTime - 100;
+        }
+        else if (num_steps < ramp_steps) {
+          delayTime = delayTime + 100;
+        }
+        else {
+          delayTime = 5000;
+        }
+      }
+      digitalWrite(en, LOW);                //Takes one step for each motor, with a delay time as defined earlier
+      if (num_steps == steps_start) {
+        timeStamp = millis();
+        PT_WAIT_UNTIL(pt, millis() - timeStamp > 250);
+      }
+
+      digitalWrite(dirPin1, motor1_dir);
+      digitalWrite(dirPin2, motor2_dir);
+      digitalWrite(stepPin1, HIGH); 
+      digitalWrite(stepPin2, HIGH);
+      timeStamp = micros();
+      PT_WAIT_UNTIL(pt, micros() - timeStamp > delayTime);
+      digitalWrite(stepPin1, LOW);
+      digitalWrite(stepPin2, LOW);
+      timeStamp = micros();
+      PT_WAIT_UNTIL(pt, micros() - timeStamp > delayTime);
+      num_steps--;                //increments the number of steps left to take
+      if (num_steps == 0) {
+        timeStamp = millis();  //adds a delay to the motor shut off.
+        PT_WAIT_UNTIL(pt, millis() - timeStamp > 500); // helps reduce drift when stopping
+        Serial.println(1);
+      }
+      if ( comm[0] != 1 && distance1 < distThresh && num_steps > ramp_steps && distance1 !=0 ) {
+        num_steps = ramp_steps;
+        steps_left = num_steps-ramp_steps;
+      }
+    }
+    //shuts off the motor drives when Flora isnt moving
+
+    digitalWrite(en, HIGH);    //saves power and wont melt the drives or wires
+
+  }
+  PT_END(pt)
+
+}
+
 
 
 void setup() {
@@ -47,133 +172,15 @@ void setup() {
   pinMode(trigPin2, OUTPUT);
   pinMode(echoPin2, INPUT);
   Serial.begin(9600);
+  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin2, LOW);
+  PT_INIT(&pt1);
+  PT_INIT(&pt2);
+  PT_INIT(&pt3);
 
 }
 void loop() {
-  digitalWrite(trigPin, LOW);
-  digitalWrite(trigPin2, LOW);
-
-
-  if (Serial.available()) {
-    byte sizeR = Serial.readBytes(receive, 31);
-    receive[sizeR] = 0;
-    char* command = strtok(receive, ",");
-    int i = 0;
-    while (command != 0)
-    {
-
-      comm[i] = atoi(command);
-      i += 1;
-      // Find the next command in input string
-      command = strtok(0, ",");
-    }
-  }
-
-  if (comm[0] == 0) {
-    if (comm[1] == 1) {
-      motor1_dir = LOW;
-      motor2_dir = HIGH;
-    }
-    else if (comm[1] == 0) {
-      motor1_dir = HIGH;
-      motor2_dir = LOW;
-    }
-  }
-  else if (comm[0] == 1) {
-    if (comm[1] == 1) {
-      motor1_dir = HIGH;
-      motor2_dir = HIGH;
-    }
-    else if (comm[1] == 0) {
-      motor1_dir = LOW;
-      motor2_dir = LOW;
-    }
-  }
-
-  num_steps = comm[2];
-  comm[2] = 0;
-  count = 1;
-  steps_start = num_steps;
-  delayTime = 15000;
-  if (num_steps < 200) {
-    ramp_steps = num_steps / 2;
-  }
-  else {
-    ramp_steps = 100;
-  }
-  timeStamp1 = micros();
-
-  while (num_steps > 0) {
-    if (comm[0] == 1) {
-      delayTime = 30000;
-    }
-    else if (comm[0] == 0) {
-      if (num_steps + ramp_steps > steps_start) {
-        delayTime = delayTime - 100;
-      }
-      else if (num_steps < ramp_steps) {
-        delayTime = delayTime + 100;
-      }
-      else {
-        delayTime = 5000;
-      }
-    }
-    digitalWrite(en, LOW);                //Takes one step for each motor, with a delay time as defined earlier
-    if (num_steps == steps_start) {
-      delay(250);
-    }
-
-    digitalWrite(dirPin1, motor1_dir);
-    digitalWrite(dirPin2, motor2_dir);
-    digitalWrite(stepPin1, HIGH);
-    digitalWrite(stepPin2, HIGH);
-    delayMicroseconds(delayTime);
-    digitalWrite(stepPin1, LOW);
-    digitalWrite(stepPin2, LOW);
-    delayMicroseconds(delayTime);
-    num_steps--;                        //increments the number of steps left to take
-    if (num_steps == 0) {
-      delay(500);
-      Serial.println(1);
-    }
-
-    //Serial.println(num_steps);
-    /*
-      if (count ==41){                    //checks for an object in front of flora and its distance on the first
-        count = 0;                      //sensor
-        digitalWrite(trigPin,HIGH);
-        delayMicroseconds(10);
-        digitalWrite(trigPin,LOW);
-
-        duration = pulseIn(echoPin,HIGH,15000);
-
-        distance1 = duration/2*0.034;
-      }
-      else if (count ==21){               //checks hte second ultrasonic sensor and the distance
-        delayMicroseconds(20);
-        digitalWrite(trigPin2,HIGH);
-        delayMicroseconds(10);
-        digitalWrite(trigPin2,LOW);
-
-        duration2 = pulseIn(echoPin2,HIGH,15000);
-
-        distance2 = duration2/2*0.034;
-      }
-
-      count++;
-      if ((distance1 <= distThresh && distance1!=0) || (distance2 <=distThresh && distance2 != 0)) {
-      steps_left = num_steps;
-      num_steps = 100;
-      Serial.println(steps_left);
-      }
-      else {
-      steps_left = num_steps;
-      }*/
-  }
-  //shuts off the motor drives when Flora isnt moving
-
-  digitalWrite(en, HIGH);             //saves power and wont melt the drives or wires
-  //Serial.println(steps_left);
-
-
+  protoMain(&pt1);
+  protoUS1(&pt2);
+  //  protoUS2(&pt3);
 }
