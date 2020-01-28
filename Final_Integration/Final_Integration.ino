2ddabd2a8d8cbe28195acff13ceb9b38a28423a7
Nicholas Bagnato <bagnatona@gmail.com>
2020-01-27 21:14:42 -0500

    extending height of arm

--- Final_Integration/Final_Integration.ino
+++ Final_Integration/Final_Integration.ino
@@ -134,7 +134,7 @@ void rotation(int water = 50) {
 // Watering Module 
 void Watering(int water) {
     
-    int angles[2] = {70, 90};
+    int angles[2] = {70, 110};
     
     shoulderAngle(P_Servo, M_Servo, 0, angles[0], stepSize);
   
